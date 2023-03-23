c492cc5fd62828b4df03e5e7fa6b8bc9e3dc3027
Rex Vincent Balde <rjbalde@pjlhuillier.com>
2023-03-23 11:46:32 +0800

    interfaces for dropdown list done

---
+++ DataInventorySystem/Controllers/RegistrationAssetController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataInventorySystem.Controllers
+{
+    public class RegistrationAssetController : Controller
+    {
+        //inputdb
+        // GET: RegistrationAsset
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Registered()
+        {
+            return View();
+        }
+    }
+}
---
+++ DataInventorySystem/Models/RegistrationAsset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataInventorySystem.Models
+{
+    public class RegistrationAsset
+    {
+        [Key]
+        public int AssetRegisterNo { get; set; }
+        public string AssetName { get; set; }
+        public string DeptServices { get; set; }
+        public string Location { get; set; }
+        public string Owner { get; set; }
+        public string Administrator { get; set; }
+        public string Classification { get; set; }
+
+    }
+}
